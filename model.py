e4a54561f9a8027acc5f35f8b9d6230ac7c6506d
zffffw <424533148@qq.com>
2019-12-29 17:05:45 +0800

    add  adv_TransE adv_DistMult.  《Embedding Entities and Relations for Learning and Inference in Knowledge Bases.》

--- model.py
+++ model.py
@@ -47,7 +47,45 @@ class TransE(nn.Module):
         batch_t = self.ent_embeddings(t)
         score = self._calc(batch_h, batch_r, batch_t)       
         return score
-        
+
+class adv_TransE(nn.Module):
+    def __init__(self, ent_tot, rel_tot, em_dim = 100):
+        super(adv_TransE, self).__init__()
+        self.name = 'adv_TransE'
+        self.dim = em_dim
+        self.ent_embeddings = nn.Embedding(ent_tot, em_dim, max_norm = 1)
+        self.Vr = nn.Embedding(rel_tot, em_dim)
+        self.W = nn.Parameter(torch.zeros((em_dim, em_dim)), requires_grad=True)
+        self.init_weights()
+    def init_weights(self):
+        nn.init.xavier_uniform_(self.ent_embeddings.weight.data)
+        nn.init.xavier_uniform_(self.Vr.weight.data)
+        nn.init.xavier_uniform_(self.W)
+    def _calc(self, batch_h, batch_r, batch_t):
+        # yh = torch.sigmoid(torch.mm(self.W, batch_h.T).T)
+        # yt = torch.sigmoid(torch.mm(self.W, batch_t.T).T)
+        yh = torch.tanh(torch.mm(self.W, batch_h.T).T)
+        yt = torch.tanh(torch.mm(self.W, batch_t.T).T)
+        # yh = torch.mm(self.W, batch_h.T).T
+        # yt = torch.mm(self.W, batch_t.T).T
+        # print(yh.shape, yt.shape, batch_r.shape)
+        return torch.norm(yh - yt + batch_r, 2, -1)
+    def forward(self, batch_h, batch_r, batch_t, batch_size):
+        batch_h = self.ent_embeddings(batch_h)
+        batch_r = self.Vr(batch_r)
+        batch_t = self.ent_embeddings(batch_t)
+        score = self._calc(batch_h, batch_r, batch_t)
+        pos_score = score[:batch_size]
+        neg_score = score[batch_size:]
+        return pos_score, neg_score
+    def predict(self, h, r, t):
+        batch_h = self.ent_embeddings(batch_h)
+        batch_r = self.Vr(batch_r)
+        batch_t = self.ent_embeddings(batch_t)
+        score = self._calc(batch_h, batch_r, batch_t)
+        return score
+
+
 
 class DistMult(nn.Module):
     def __init__(self, ent_tot, rel_tot, em_dim = 50):
@@ -97,13 +135,52 @@ class DistMult(nn.Module):
         
         return score
 
+class adv_DistMult(nn.Module):
+    def __init__(self, ent_tot, rel_tot, em_dim = 100):
+        super(adv_DistMult, self).__init__()
+        self.ent_embeddings = nn.Embedding(ent_tot, em_dim, max_norm = 1)
+        self.Vr = nn.Embedding(rel_tot, em_dim)
+        self.W = nn.Parameter(torch.zeros((em_dim, em_dim)), requires_grad=True)
+        self.init_weights()
+    def init_weights(self):
+        nn.init.xavier_uniform_(self.ent_embeddings.weight.data)
+        nn.init.xavier_uniform_(self.Vr.weight.data)
+        nn.init.xavier_uniform_(self.W)
+    def _calc(self, batch_h, batch_r, batch_t):
+        # yh = torch.sigmoid(torch.mm(self.W, batch_h.T).T)
+        # yt = torch.sigmoid(torch.mm(self.W, batch_t.T).T)
+        yh = torch.tanh(torch.mm(self.W, batch_h.T).T)
+        yt = torch.tanh(torch.mm(self.W, batch_t.T).T)
+        # yh = torch.mm(self.W, batch_h.T).T
+        # yt = torch.mm(self.W, batch_t.T).T
+        # print(yh.shape, yt.shape, batch_r.shape)
+        return torch.sum(yh * yt * batch_r, -1)
+    def forward(self, batch_h, batch_r, batch_t, batch_size):
+        batch_h = self.ent_embeddings(batch_h)
+        batch_r = self.Vr(batch_r)
+        batch_t = self.ent_embeddings(batch_t)
+        score = self._calc(batch_h, batch_r, batch_t)
+        pos_score = score[:batch_size]
+        neg_score = score[batch_size:]
+        return pos_score, neg_score
+    def predict(self, h, r, t):
+        batch_h = self.ent_embeddings(batch_h)
+        batch_r = self.Vr(batch_r)
+        batch_t = self.ent_embeddings(batch_t)
+        score = self._calc(batch_h, batch_r, batch_t)
+        return score
+    
+
 
 if __name__=='__main__':
-    model = transE(100, 100, init_embed=True)
-    h = torch.LongTensor([1])
-    r = torch.LongTensor([1])
-    t = torch.LongTensor([1])
-    a = model(h, r, t)
+    model = adv_TransE(5, 5, 3)
+    # print(model.W)
+    # print(model.ent_embeddings(torch.LongTensor([1])))
+    h = torch.LongTensor([1, 2])
+    r = torch.LongTensor([1, 3])
+    t = torch.LongTensor([1, 2])
+    model(h, r, t, 2)
+    # a = model(h, r, t)
 
 
 
